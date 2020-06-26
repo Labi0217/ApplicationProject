f6f5d35a0d45668510e773aa7e5eb69cfed316b8
Labi0217 <wlsglckals@gmail.com>
2020-06-26 14:52:22 +0900

    initial commit

--- app/src/main/java/com/sohee/applicationproject/AddPlace.java
+++ app/src/main/java/com/sohee/applicationproject/AddPlace.java
@@ -24,13 +24,13 @@ public class AddPlace extends AppCompatActivity {
     private DBHelper mydb;
     TextView name;
     TextView director;
-    TextView country;
+    TextView area;
     TextView classify;
-    TextView nation;
-    TextView rating;
+    TextView content;
+    TextView cost;
     Spinner spinner;
     Spinner spinner2;
-    TextView countryreal;
+    TextView areareal;
     TextView classifyreal;
     int id = 0;
     private static final int REQUEST_CODE = 0;
@@ -44,14 +44,14 @@ public class AddPlace extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place);
-        countryreal = (TextView)findViewById(R.id.textView1);
+        areareal = (TextView)findViewById(R.id.textView1);
         classifyreal = (TextView)findViewById(R.id.textView4);
         name = (TextView) findViewById(R.id.editTextLocation);
         director = (TextView) findViewById(R.id.editTextName);
-        country = (TextView)findViewById(R.id.TextCountry);
+        area = (TextView)findViewById(R.id.TextArea);
         classify = (TextView)findViewById(R.id.TextClassify);
-        nation = (TextView) findViewById(R.id.editTextContent);
-        rating = (TextView) findViewById(R.id.editTextCost);
+        content = (TextView) findViewById(R.id.editTextContent);
+        cost = (TextView) findViewById(R.id.editTextCost);
         imageView = findViewById(R.id.image);
         spinner = findViewById(R.id.spinner);
         spinner2 = findViewById(R.id.spinner2);
@@ -78,9 +78,9 @@ public class AddPlace extends AppCompatActivity {
                 rs.moveToFirst();
                 String n = rs.getString(rs.getColumnIndex(DBHelper.NOTE_LOCATION));
                 String d = rs.getString(rs.getColumnIndex(DBHelper.NOTE_NAME));
-                String c = rs.getString(rs.getColumnIndex(DBHelper.NOTE_AREA));
-                String na = rs.getString(rs.getColumnIndex(DBHelper.NOTE_CONTENT));
-                String r = rs.getString(rs.getColumnIndex(DBHelper.NOTE_COST));
+                String a = rs.getString(rs.getColumnIndex(DBHelper.NOTE_AREA));
+                String con = rs.getString(rs.getColumnIndex(DBHelper.NOTE_CONTENT));
+                String co = rs.getString(rs.getColumnIndex(DBHelper.NOTE_COST));
                 if (!rs.isClosed()) {
                     rs.close();
                 }
@@ -89,9 +89,9 @@ public class AddPlace extends AppCompatActivity {
 
                 name.setText((CharSequence) n);
                 director.setText((CharSequence) d);
-                country.setText((CharSequence)c);
-                nation.setText((CharSequence) na);
-                rating.setText((CharSequence) r);
+                area.setText((CharSequence)a);
+                content.setText((CharSequence) con);
+                cost.setText((CharSequence) co);
             }
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
@@ -108,37 +108,37 @@ public class AddPlace extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-                countryreal.setText(items[position]);
+                areareal.setText(items[position]);
                 if(items[position] == "서울특별시") {
-                    country.setText("서울특별시");
+                    area.setText("서울특별시");
                 } else if(items[position] == "인천광역시") {
-                    country.setText("인천광역시");
+                    area.setText("인천광역시");
                 } else if(items[position] == "울산광역시") {
-                    country.setText("울산광역시");
+                    area.setText("울산광역시");
                 } else if(items[position] == "충청북도") {
-                    country.setText("충청북도");
+                    area.setText("충청북도");
                 } else if(items[position] == "충청남도") {
-                    country.setText("충청남도");
+                    area.setText("충청남도");
                 } else if(items[position] == "전라북도") {
-                    country.setText("전라북도");
+                    area.setText("전라북도");
                 } else if(items[position] == "전라남도") {
-                    country.setText("전라남도");
+                    area.setText("전라남도");
                 } else if(items[position] == "광주광역시") {
-                    country.setText("광주광역시");
+                    area.setText("광주광역시");
                 } else if(items[position] == "부산광역시") {
-                    country.setText("부산광역시");
+                    area.setText("부산광역시");
                 } else if(items[position] == "경기도") {
-                    country.setText("경기도");
+                    area.setText("경기도");
                 } else if(items[position] == "경상북도") {
-                    country.setText("경상남도");
+                   area.setText("경상남도");
                 } else if(items[position] == "대구광역시") {
-                    country.setText("대구광역시");
+                    area.setText("대구광역시");
                 } else if(items[position] == "대전광역시") {
-                    country.setText("대전광역시");
+                    area.setText("대전광역시");
                 } else if(items[position] == "강원도") {
-                    country.setText("강원도");
+                    area.setText("강원도");
                 } else if(items[position] == "제주특별자치도") {
-                    country.setText("제주특별자치도");
+                    area.setText("제주특별자치도");
                 }
 
 
@@ -147,7 +147,7 @@ public class AddPlace extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                country.setText("");
+                area.setText("");
             }
         });
     }
@@ -159,7 +159,7 @@ public class AddPlace extends AppCompatActivity {
         if (extras != null) {
             int Value = extras.getInt("id");
             if (Value > 0) {
-                if (mydb.updateNote(id, country.getText().toString(), name.getText().toString(), director.getText().toString(), nation.getText().toString(), rating.getText().toString())) {
+                if (mydb.updateNote(id, area.getText().toString(), name.getText().toString(), director.getText().toString(), content.getText().toString(), cost.getText().toString())) {
                     Toast.makeText(getApplicationContext(), "수정되었음", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(getApplicationContext(), StoragePlace.class);
                     startActivity(intent);
@@ -167,7 +167,7 @@ public class AddPlace extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "수정되지 않았음", Toast.LENGTH_SHORT).show();
                 }
             } else {
-                if (mydb.insertNote(country.getText().toString(), name.getText().toString(), director.getText().toString(), nation.getText().toString(), rating.getText().toString())) {
+                if (mydb.insertNote(area.getText().toString(), name.getText().toString(), director.getText().toString(), content.getText().toString(), cost.getText().toString())) {
                     Toast.makeText(getApplicationContext(), "추가되었음", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(getApplicationContext(), "추가되지 않았음", Toast.LENGTH_SHORT).show();
@@ -196,7 +196,7 @@ public class AddPlace extends AppCompatActivity {
         if (extras != null) {
             int value = extras.getInt("id");
             if (value > 0) {
-                if (mydb.updateNote(id, country.getText().toString(), name.getText().toString(), director.getText().toString(), nation.getText().toString(), rating.getText().toString())) {
+                if (mydb.updateNote(id, area.getText().toString(), name.getText().toString(), director.getText().toString(), content.getText().toString(), cost.getText().toString())) {
                     Toast.makeText(getApplicationContext(), "수정되었음", Toast.LENGTH_SHORT).show();
                     finish();
                 } else {
